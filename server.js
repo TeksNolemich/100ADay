57ad074dcc7eb56cd1d825e30f1df8874fcf4d0b
Arthur Tharp <atharp@uci.edu>
2019-02-28 12:14:03 -0800

    React added to project

--- server.js
+++ server.js
@@ -2,5 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use(express.static(__dirname + '/public/client'));
+app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!, `, __dirname),
+);
